0f49b60d5dde703cee67bde240858c322127272e
Oluwasegun Olawale Benjamin <benjaminoluwasegun@gmail.com>
2023-01-05 20:28:04 -0800

    Task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,15 @@
-if __name__ == "__main__":
-    import sys
+#!/usr/bin/python3
+from sys import argv
 
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+argc = len(argv)
+
+if __name__ == "__main__":
+    if argc == 1:
+        print("{:d} arguments.".format(argc - 1))
+    elif argc == 2:
+        print("{:d} argument:".format(argc - 1))
     else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{:d} arguments:".format(argc - 1))
+
+    for i, arg in enumerate(argv[1:], 1):
+        print("{:d}: {}".format(i, arg))
